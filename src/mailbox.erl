50a979a95c18c851545ea688c88c29b5141ad2f7
Kevin Buchanan <kevaustinbuch@gmail.com>
2015-01-16 21:16:28 -0600

    adds listeners to mailbox

--- src/mailbox.erl
+++ src/mailbox.erl
@@ -2,18 +2,27 @@
 
 -export([start/1, loop/1]).
 
--record(box_state, {id, messages = []}).
+-record(box_state, {id, messages = [], listeners = []}).
 
-loop(#box_state{messages = Messages} = State) ->
+loop(#box_state{messages = Messages, listeners = Listeners} = State) ->
     receive
         {Sender, {mail, check_mailbox}} ->
             Sender ! {self(), State},
             proc_lib:hibernate(?MODULE, loop, [State]);
+
         {Sender, {mail, add_message, Message}} ->
             NewState = State#box_state{messages = [Message | Messages]},
             Sender ! {self(), NewState},
-            proc_lib:hibernate(?MODULE, loop, [NewState])
+            lists:map(fun(Pid) -> Pid ! [Message] end, Listeners),
+            proc_lib:hibernate(?MODULE, loop, [NewState]);
+
+        {Sender, {mail, add_listener, Listener}} ->
+            NewState = State#box_state{listeners = [Listener | Listeners]},
+            Sender ! {self(), NewState},
+            proc_lib:hibernate(?MODULE, loop, [NewState]);
+
+        _ -> proc_lib:hibernate(?MODULE, loop, [State])
     end.
 
-start(Id) ->
-    proc_lib:hibernate(?MODULE, loop, [#box_state{id = Id}]).
+start(SessionId) ->
+    proc_lib:hibernate(?MODULE, loop, [#box_state{id = SessionId}]).
--- test/mailbox_test.erl
+++ test/mailbox_test.erl
@@ -5,16 +5,33 @@ new_mailbox_test() ->
     Pid = spawn_link(mailbox, start, [1]),
     Pid ! {self(), {mail, check_mailbox}},
     receive
-        {Pid, {box_state, BoxId, Messages}} ->
+        {Pid, {box_state, BoxId, Messages, _}} ->
             ?assertEqual(1, BoxId),
             ?assertEqual([], Messages)
     end.
 
 add_message_test() ->
-    Pid = spawn_link(mailbox, start, [2]),
+    Pid = spawn_link(mailbox, start, [1]),
     Pid ! {self(), {mail, add_message, "Hello"}},
     receive
-        {Pid, {box_state, BoxId, Messages}} ->
-            ?assertEqual(2, BoxId),
+        {Pid, {box_state, BoxId, Messages, _}} ->
+            ?assertEqual(1, BoxId),
+            ?assertEqual(["Hello"], Messages)
+    end.
+
+add_listener_test() ->
+    Pid = spawn_link(mailbox, start, [1]),
+    Pid ! {self(), {mail, add_listener, self()}},
+    receive
+        {Pid, {box_state, _BoxId, _Messages, Listeners}} ->
+            ?assertEqual([self()], Listeners)
+    end.
+
+notify_listener_test() ->
+    Pid = spawn_link(mailbox, start, [1]),
+    Pid ! {self(), {mail, add_listener, self()}},
+    Pid ! {self(), {mail, add_message, "Hello"}},
+    receive
+        Messages when is_list(Messages) ->
             ?assertEqual(["Hello"], Messages)
     end.
